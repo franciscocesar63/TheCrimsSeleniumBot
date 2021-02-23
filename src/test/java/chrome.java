2d991bc49536f09a8694c20e9f191e4d1655bc89
Cesar <franciscocesar888@gmail.com>
2021-02-23 15:39:15 -0300

    Bot The Crims

---
+++ src/test/java/chrome.java
@@ -0,0 +1,18 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chrome 
+ {
+
+  public static void main(String[] args) {
+
+       System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+
+
+    driver.get("http://www.google.com");
+
+  }
+
+ }
